7ddb8ee47465f6f987032508c3aa1f0c85c724fb
Karthick-407-300 <karthi500and300@gmail.com>
2026-04-30 18:26:54 +0530

    added css and js content

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showMsg() {
+    alert("🚀 Jenkins + Ansible deployment working!");
+}
+
+// Auto message on page load
+window.onload = function() {
+    console.log("Page loaded from Nginx via Ansible!");
+};
